9a3652ed29ed71288cc2f37fe48864cfaf48e5d4
Blayner <blayner0027@gmail.com>
2021-05-13 16:41:56 +0400

    Добавьте файлы проекта.

---
+++ College_GeneratorAccounts/Model/Account.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace College_GeneratorAccounts.Model
+{
+    internal class Account
+    {
+        public Account(string login, string email, string password)
+        {
+            Login = login ?? throw new ArgumentNullException(nameof(login));
+            Email = email ?? throw new ArgumentNullException(nameof(email));
+            Password = password ?? throw new ArgumentNullException(nameof(password));
+        }
+
+        public string Login { get; init; }
+        public string Email { get; init; }
+        public string Password { get; init; }
+
+        public static Account GetnerateAccount(string fullName)
+        {
+            var nameToLatin = Generator.GetCollectionTranslitToLatin(fullName);
+            return new Account(Generator.GenerateLogin(nameToLatin), Generator.GenerateEmails(nameToLatin), Generator.GeneratePassword());
+        }
+    }
+}
---
+++ College_GeneratorAccounts/Model/Generator.cs
@@ -0,0 +1,46 @@
+﻿namespace College_GeneratorAccounts.Model
+{
+    /// <summary>
+    /// Генератор аккаунтов
+    /// </summary>
+    internal static class Generator
+    {
+        /// <summary>
+        /// Переводит транслитом из кириллицы в латиницу
+        /// </summary>
+        /// <param name="fullName">Коллекция, которую нужно перевести</param>
+        /// <returns>Переведённую коллекцию</returns>
+        public static string GetCollectionTranslitToLatin(string fullName)
+        {
+            //TODO: Написать траслитор
+            return fullName;
+        }
+
+        /// <summary>
+        /// Генерация почты
+        /// </summary>
+        /// <param name="fullName">Полное имя на латинице</param>
+        /// <returns>Почта</returns>
+        public static string GenerateEmails(string fullName)
+        {
+            string[] fullNameArray = fullName.Split();
+            return $"{fullNameArray[0]}.{fullNameArray[1]}@moodle.edu";
+        }
+
+        /// <summary>
+        /// Генерация логина
+        /// </summary>
+        /// <param name="nameLatin">Полное имя на латинском</param>
+        /// <returns>Логин</returns>
+        public static string GenerateLogin(string nameLatin)
+        {
+            string[] nameLatinArray = nameLatin.Split();
+            return $"{nameLatinArray[0]}_{nameLatinArray[1]}";
+        }
+
+        public static string GeneratePassword()
+        {
+            return string.Empty;
+        }
+    }
+}
