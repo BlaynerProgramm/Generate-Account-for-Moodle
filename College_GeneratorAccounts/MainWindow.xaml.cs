14194a0eae6be92a21e2694424a56b1cf1921875
Kirill <blayner0027@gmail.com>
2021-05-16 16:19:37 +0400

    Добавил функционал импорта и экспорта

--- College_GeneratorAccounts/MainWindow.xaml.cs
+++ College_GeneratorAccounts/MainWindow.xaml.cs
@@ -1,40 +1,50 @@
 ﻿using College_GeneratorAccounts.Model;
 
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace College_GeneratorAccounts
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		private string[] data;
+		private readonly List<Account> accounts = new();
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
 
-        private void BtGenerateAccount_Click(object sender, RoutedEventArgs e)
-        {
-            
-        }
+		private void BtGenerateAccount_Click(object sender, RoutedEventArgs e)
+		{
+			for (int i = 0; i < data.Length; i++)
+			{
+				accounts.Add(Account.GetnerateAccount(data[i]));
+			}
+			tb.Text = "";
+			foreach (Account account in accounts)
+			{
+				tb.Text += account;
+			}
+		}
 
-        private void BtImport_Click(object sender, RoutedEventArgs e)
-        {
+		private void BtImport_Click(object sender, RoutedEventArgs e)
+		{
+			data = ImportData.GetData();
+			for (int i = 0; i < data.Length; i++)
+			{
+				tb.Text += data[i];
+			}
+		}
 
-        }
-    }
+		private void BtExport_Click(object sender, RoutedEventArgs e)
+		{
+			
+			MessageBox.Show("Ok");
+		}
+	}
 }
+
--- College_GeneratorAccounts/Model/Account.cs
+++ College_GeneratorAccounts/Model/Account.cs
@@ -2,23 +2,28 @@
 
 namespace College_GeneratorAccounts.Model
 {
-    internal class Account
-    {
-        public Account(string login, string email, string password)
-        {
-            Login = login ?? throw new ArgumentNullException(nameof(login));
-            Email = email ?? throw new ArgumentNullException(nameof(email));
-            Password = password ?? throw new ArgumentNullException(nameof(password));
-        }
+	internal class Account
+	{
+		public Account(string login, string email, string password)
+		{
+			Login = login ?? throw new ArgumentNullException(nameof(login));
+			Email = email ?? throw new ArgumentNullException(nameof(email));
+			Password = password ?? throw new ArgumentNullException(nameof(password));
+		}
 
-        public string Login { get; init; }
-        public string Email { get; init; }
-        public string Password { get; init; }
+		public string Login { get; init; }
+		public string Email { get; init; }
+		public string Password { get; init; }
 
-        public static Account GetnerateAccount(string fullName)
-        {
-            var nameToLatin = Generator.GetCollectionTranslitToLatin(fullName);
-            return new Account(Generator.GenerateLogin(nameToLatin), Generator.GenerateEmails(nameToLatin), Generator.GeneratePassword());
-        }
-    }
+		public static Account GetnerateAccount(string fullName)
+		{
+			string nameToLatin = Generator.GetCollectionTranslitToLatin(fullName);
+			return new Account(Generator.GenerateLogin(nameToLatin), Generator.GenerateEmails(nameToLatin), Generator.GeneratePassword());
+		}
+
+		public string ToStringCsv() => $"{Login} {Email} {Password};";
+
+
+		public override string ToString() => $"Login: {Login}\nEmail: {Email}\nPassword: {Password}\n";
+	}
 }
